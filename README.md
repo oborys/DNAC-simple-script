d2bb798eff9ed786d126a3f3a4931c5c4e9a8abc
Oleksii Borisenko <borysenko.oleksii@gmail.com>
2022-11-16 23:24:50 +0100

    update files

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# DNAC-simple-script
+# Cisco DNA Center Device List, and Network Health
+
+This script is designed to show simple REST API operation and print Cisco DNAC device list and network health
+
+# Deploy in Cisco Exchange Dev environment
+
+If you run this project using the Cisco Exchange Dev environment
+
+![automation-scripts-exchange-devenv](https://raw.githubusercontent.com/CiscoDevNet/code-exchange-repo-template/master/manual-sample-repo/img/automation-scripts-exchange-devenv.png)
+
+Scripts using [Cisco DNA Center Always-on sandbox](https://devnetsandbox.cisco.com/RM/Diagram/Index/c3c949dc-30af-498b-9d77-4f1c07d835f9?diagramType=Topology)
+
+In the Cisco Exchange Dev environment, you can try with the following commands:
+
+Install Python packages
+
+```
+pip install -r requirements.txt
+```
+
+Run `simple_api_operation_dnac.py`
+
+```
+python simple_api_operation_dnac.py
+
+```
+
+Sample output:
+```
+ Extract related information from JSON:
+
+ID:  6b741b27-f7e7-4470-b6fc-d5168cc59502  Hostname  c3504.abc.inc  Management IP Address  10.10.20.51  Serial Number  FOL25040021
+ID:  aa0a5258-3e6f-422f-9c4e-9c196db115ae  Hostname  leaf1.abc.inc  Management IP Address  10.10.20.81  Serial Number  FCW2220G09V
+ID:  f0cb8464-1ce7-4afe-9c0d-a4b0cc5ee84c  Hostname  leaf2.abc.inc  Management IP Address  10.10.20.82  Serial Number  FCW2211G0MA
+ID:  f16955ae-c349-47e9-8e8f-9b62104ab604  Hostname  spine1.abc.inc  Management IP Address  10.10.20.80  Serial Number  FOC2135Z00T
+
+
+Health Score 75, Total Count: 4
+Category Access, Health Score: 100
+Category Distribution, Health Score: 100
+Category WLC, Health Score: 0
+```
+
+# For deploy on your local machine
+
+```
+Terminal command here
+```
+You will copy this text and paste it into the terminal. 
+
+For Linux:
+
+### Copy - CTRL+INSERT
+### Paste - SHIFT+INSERT
+
+Clone the repo
+
+```
+git clone https://github.com/oborys/DNAC-simple-script
+```
+
+Work with Python:
+
+```
+cd DNAC-simple-script
+```
+
+### Setup virtualenvironment
+
+**Install the virtualenv package**
+
+The virtualenv package is required to create virtual environments. You can install it with pip:
+```
+pip install virtualenv
+```
+**Create the virtual environment**
+
+To create a virtual environment, you must specify a path. For example to create one in the local directory called `py3-venv`, type the following:
+```
+python3 -m venv py3-venv
+```
+
+**"Activate" the environment**
+
+Mac OS / Linux
+```
+source py3-venv/bin/activate
+```
+
+Windows
+```
+py3-venv\Scripts\activate
+```
+
+**Install packages (list of packages in file `requirements.txt`) into the current python environment**
+```
+pip install -r requirements.txt 
+```
+
+**Run script**
+
+```
+python simple_api_operation_http_errors.py
+```
+
+**Deactivate the virtual environment**
+
+To decativate the virtual environment and use your original Python environment, simply type `deactivate`.
+
+```
+deactivate
+```
---
+++ simple_api_operation_dnac.py
@@ -0,0 +1,89 @@
+import requests
+from requests.exceptions import Timeout
+import base64
+
+requests.packages.urllib3.disable_warnings()
+
+USER_DNA = "devnetuser"
+PASSWORD_DNA = "Cisco123!"
+DEF_TIMEOUT = 10
+
+tokenDNA = ''
+
+def main():
+    API_Server = "https://sandboxdnac.cisco.com/"
+    #  functions
+    sandboxAvailability(API_Server)
+    checkSimpleRequest(API_Server)
+    networkHealth(API_Server)
+
+
+
+def sandboxAvailability(API_Server):
+    response = requests.get(API_Server)
+    if response.status_code != 200:
+        #send notification to bot
+        print("Error", response.status_code)
+        exit()
+    print("response.status_code (HTTP Status code): ", response.status_code)
+    #print("response.content: ", response.content[:50])
+    #print("response.text: ", response.text[:50])
+    return (response.status_code)
+
+def checkSimpleRequest(API_Server):
+    global tokenDNA
+    API_Endpoint = API_Server + "api/system/v1/"
+    Path = "auth/token"
+    API_Resource =  API_Endpoint + Path
+    usrPasDna = USER_DNA + ":" + PASSWORD_DNA
+    basicDNA = base64.b64encode(usrPasDna.encode()).decode()
+    HTTP_Request_header = {"Authorization": "Basic %s" % basicDNA,
+                "Content-Type": "application/json;"}
+    body_json = ""
+
+    try:
+        # API Operation
+        response = requests.post(API_Resource, data=body_json, headers=HTTP_Request_header, verify=False, timeout=DEF_TIMEOUT)
+    except Timeout as e:
+        raise Timeout(e)
+    tokenDNA = response.json()['Token']
+    urlSimpleDNA = API_Server + "api/v1/network-device/"
+    urlSimpleDNAerror = API_Server + "api/v1/network-devic/"
+    HTTP_Request_header = {'x-auth-token': tokenDNA}
+    try:
+        response = requests.get(urlSimpleDNA, headers=HTTP_Request_header)
+        print ("\n API Operation: GET https://sandboxdnac.cisco.com/api/v1/network-device/ response JSON:\n", response.json())
+        print ("\n Extract related information from JSON:\n")
+        for item in response.json()['response']:
+            print("ID: ", item['id'], " Hostname ", item['hostname'], " Management IP Address ", item['managementIpAddress'], " Serial Number ", item['serialNumber'])
+        if response.status_code == 429:
+            retry_after = response.getheader("Retry-After")
+            time.sleep(retry_after)
+            #time.sleep(30)
+        if response.status_code != 200:
+            print("Error SimpleRequest status_code != 200")
+            exit()
+    except Timeout as e:
+        raise Timeout(e)
+
+def networkHealth(API_Server):
+    API_Endpoint = API_Server + 'dna/intent/api/v1/'
+    Path = "network-health"
+    API_Resource =  API_Endpoint + Path
+    usrPasDna = USER_DNA + ":" + PASSWORD_DNA
+    basicDNA = base64.b64encode(usrPasDna.encode()).decode()
+    HTTP_Request_header = {'x-auth-token': tokenDNA}
+    try:
+        # API Operation
+        response = requests.get(API_Resource, headers=HTTP_Request_header, verify=False, timeout=DEF_TIMEOUT)
+        print('\n')
+        for networkhealth in response.json()['response']:
+            print('Health Score {0}, Total Count: {1}'.format(networkhealth['healthScore'], networkhealth['totalCount']))
+        for hd in response.json()["healthDistirubution"]:
+            print('Category {0}, Health Score: {1}'.format(hd["category"], hd['healthScore']))
+    
+    except Timeout as e:
+        raise Timeout(e)
+
+if __name__ == "__main__":
+    main()
